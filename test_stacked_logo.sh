6f73b2a62a83753da37a02cd877d182dbd1d8ec7
Kristian Davidsen <krdav@cbs.dtu.dk>
2017-05-17 15:11:17 -0700

    organized test folder

---
+++ test_stacked_logo.sh
@@ -0,0 +1,6 @@
+
+python stacked_logo.py stacked_logo_test/1N/dff2.subsamp.txt stacked_logo_test/1N/KD_test2/dff2.pred.txt --full_profile stacked_logo_test/1N/KD_test2/dff2.full.txt
+
+python stacked_logo.py stacked_logo_test/10N/dff2.subsamp.txt stacked_logo_test/10N/KD_test2/dff2.pred.txt --full_profile stacked_logo_test/10N/KD_test2/dff2.full.txt
+
+
